e1cd59ea9df8f655671b5a48fd4dda5e570ea83e
Ilya <osetia9595@gmail.com>
2023-01-16 17:03:23 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("input rows count");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("input columns count");
+int n = int.Parse(Console.ReadLine());
+int [,] array = new int [m, n];
+FillArray(array);
+PrintArray(array);
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.Write($"average is  { sum / array.GetLength(0)} ");
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100);
+            }   
+        }
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.WriteLine(""); 
+    }
+}
