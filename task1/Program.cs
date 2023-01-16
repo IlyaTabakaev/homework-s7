5b4258823acadb5395ad9629158bb775d12d2022
Ilya <osetia9595@gmail.com>
2023-01-16 16:14:20 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("input M");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("input N");
+int n = int.Parse(Console.ReadLine());
+
+double [,] array = new double[m, n];
+FillArray(array);
+PrintArray(array);
+
+void PrintArray(double[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write($"{array[m, n]} ");
+        }
+    }
+}
+
+void FillArray(double[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            array[m, n] = new Random().Next(10, 100);
+        }
+    
+}
+
+}
